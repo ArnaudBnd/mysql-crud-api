77e787552658b92c9aa3ba3ffc024d37fe40d5c5
arnaud <arnaudbenede@hotmail.fr>
2019-07-03 14:06:47 +0200

    add babel watch to try

--- src/config/dbConnection.js
+++
@@ -1,9 +0,0 @@
-// connection configurations
- var dbConn = mysql.createConnection({
-     host: 'localhost',
-     user: 'root',
-     password: '',
-     database: 'mysql-crud'
- });
- // connect to database
- dbConn.connect(); 
