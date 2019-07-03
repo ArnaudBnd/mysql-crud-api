7d936bda809b9a73f9b68909a0de699481086042
arnaud <arnaudbenede@hotmail.fr>
2019-07-03 12:19:36 +0200

    add starter

---
+++ src/config/dbConnection.js
@@ -0,0 +1,9 @@
+// connection configurations
+ var dbConn = mysql.createConnection({
+     host: 'localhost',
+     user: 'root',
+     password: '',
+     database: 'mysql-crud'
+ });
+ // connect to database
+ dbConn.connect(); 
